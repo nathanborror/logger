d20349185ca0f925f73b59f3ac29fb667fd3b5d9
Nathan Borror <nathan@nathanborror.com>
2018-03-26 17:07:50 -0700

    Fixing increment build

--- Scripts/increment-build.sh
+++ Scripts/increment-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${PROJECT_DIR}/${INFOPLIST_FILE}")
+buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${INFOPLIST_FILE}")
 buildNumber=$(($buildNumber + 1))
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${PROJECT_DIR}/${INFOPLIST_FILE}"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${INFOPLIST_FILE}"
