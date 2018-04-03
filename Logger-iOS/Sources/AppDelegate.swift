3adb5e2899378945455fa2f93db6acb09e64b2c2
Nathan Borror <nathan@nathanborror.com>
2018-04-02 23:03:31 -0700

    Setting up UIWindow in application will finish launching

--- Logger-iOS/Sources/AppDelegate.swift
+++ Logger-iOS/Sources/AppDelegate.swift
@@ -6,11 +6,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(_ application: UIApplication,
+                     willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = .white
         window?.rootViewController = EntriesVC()
         window?.makeKeyAndVisible()
+        return true
+    }
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         return true
     }
 
